fc7164ec42069953f793b1f26976444a7851b136
Richard Xu <richard.xu@roadbotics.com>
2018-05-08 14:57:03 -0400

    Add todo-list custom element

---
+++ src/components/TodoList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="text">
+      <button>Add todo</button>
+    </form>
+    <ul>
+      <li 
+        :key="todo.id" 
+        @click="deleteTodo(todo.id)" 
+        v-for="todo in todos">{{ todo.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    text: '',
+    todos: [],
+  }),
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: Date.now(),
+        text: this.text
+      }) 
+      this.text = ''
+    },
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+} */
+</style>
--- src/main.js
+++ src/main.js
@@ -5,5 +5,8 @@ import VueCustomElement from 'vue-custom-element'
 Vue.use(VueCustomElement)
 
 // import and register your component(s)
-import ShareButtonsComponent from './components/ShareButtonsComponent'
-Vue.customElement('share-buttons', ShareButtonsComponent)
+// import ShareButtonsComponent from './components/ShareButtonsComponent'
+import TodoList from './components/TodoList'
+
+// Vue.customElement('share-buttons', ShareButtonsComponent)
+Vue.customElement('todo-list', TodoList)
